e77409e717dcbc7d3c0839c0d2988be249dba6b0
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-06 22:56:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan"
+var height = "70"
 
 
 // Don't worry about this
 module.exports = { name, height
 }
 
-if (name == "Susan"){
-  console.log()
-}
